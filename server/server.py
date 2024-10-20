542751ad52bdc9dcc077d34becb2f2b9a89342fc
JavadiSM <1remehr@gmail.com>
2024-10-20 14:49:36 +0330

    server completly implemented. now we shall modify and customize functions

--- server/server.py
+++ server/server.py
@@ -2,19 +2,122 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-# Example functions
-def function1(inputs):
 
-    return sum(map(int, inputs))
+def regMul(*argc, **arkw):
+    pass
 
-def function2(inputs):
-    return min(map(int, inputs))
+def regMulII(*argc, **arkw):
+    pass
+
+def divGame(*argc, **arkw):
+    pass
+
+def divGameII(*argc, **arkw):
+    pass
+
+def MixedArr(*argc, **arkw):
+    pass
+
+def MixedArrII(*argc, **arkw):
+    pass
+
+def polyEval(*argc, **arkw):
+    pass
+
+def DetGame(*argc, **arkw):
+    pass
+
+def EigenValGame(*argc, **arkw):
+    pass
+
+def DiscGame(*argc, **arkw):
+    pass
+
+def rootGame(*argc, **arkw):
+    pass
+
+def PFD(*argc, **arkw):
+    pass
+
+def IntegralGame(*argc, **arkw):
+    pass
+
+def regMulDig(*argc, **arkw):
+    pass
+
+def FourierSeries(*argc, **arkw):
+    pass
+
+def EquationSystem(*argc, **arkw):
+    pass
+
+def Mean(*argc, **arkw):
+    pass
+
+def Stdev(*argc, **arkw):
+    pass
+
+def diffeq(*argc, **arkw):
+    pass
+
+def PolyDet(*argc, **arkw):
+    pass
+
+def PolyDetFourier(*argc, **arkw):
+    pass
+
+def curvatureGame(*argc, **arkw):
+    pass
+
+def TGame(*argc, **arkw):
+    pass
+
+def LineIntegralGame(*argc, **arkw):
+    pass
+
+def DiverganceGame(*argc, **arkw):
+    pass
+
+def LineIntegralSc(*argc, **arkw):
+    pass
+
+# when anything was going wrong, use these two for test
+# def function1(inputs):
+#
+#     return sum(map(int, inputs))
+#
+# def function2(inputs):
+#     return min(map(int, inputs))
 
 function_map = {
-    "Function1": function1,
-    "Function2": function2,
-    # to be added
+    "regMul": regMul,
+    "regMulII": regMulII,
+    "divGame": divGame,
+    "divGameII": divGameII,
+    "MixedArr": MixedArr,
+    "MixedArrII": MixedArrII,
+    "polyEval": polyEval,
+    "DetGame": DetGame,
+    "EigenValGame": EigenValGame,
+    "DiscGame": DiscGame,
+    "rootGame": rootGame,
+    "PFD": PFD,
+    "IntegralGame": IntegralGame,
+    "regMulDig": regMulDig,
+    "FourierSeries": FourierSeries,
+    "EquationSystem": EquationSystem,
+    "Mean": Mean,
+    "Stdev": Stdev,
+    "diffeq": diffeq,
+    "PolyDet": PolyDet,
+    "PolyDetFourier": PolyDetFourier,
+    "curvatureGame": curvatureGame,
+    "TGame": TGame,
+    "LineIntegralGame": LineIntegralGame,
+    "DiverganceGame": DiverganceGame,
+    "LineIntegralSc": LineIntegralSc
 }
+
 
 @app.route('/execute', methods=['POST'])
 def execute():
